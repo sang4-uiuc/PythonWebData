a187ef9a077d3d3955c14d46cd681789345bfff5
Thomas Sang <sang4@illinois.edu>
2016-05-24 15:01:04 -0400

    updated

--- week2assignment.py
+++ week2assignment.py
@@ -1,13 +1,18 @@
-import socket
+import re
 
-mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-mysock.connect(('www.pythonlearn.com', 80))
-mysock.send('GET http://www.pythonlearn.com/code/intro-short.txt HTTP/1.0\n\n')
 
-while True:
-    data = mysock.recv(512)
-    if ( len(data) < 1 ) :
-        break
-    print data;
-
-mysock.close()
+fh = raw_input("Enter filename: ")
+#returns regex_sum_241882.txt as default when nothing is entered
+if len(fh)<1 : fh = "regex_sum_241882.txt"
+file = open(fh)
+sums = list()
+#goes through each line in the file
+for line in file:
+	#finds the numbers in each line and puts them in a list
+	nums = re.findall('[0-9]+',line)
+	#adds the numbers to an existing list
+	for num in nums:
+		
+		sums.append(int(num))
+#sums the list
+print sum(sums)
